e0664421241d91922e4c58984997476b6f1e3726
dongaCS <109496734+dongaCS@users.noreply.github.com>
2024-08-25 20:12:44 -0400

    working GET

--- roast/index.js
+++ roast/index.js
@@ -1,17 +1,29 @@
 const express = require(`express`);
-const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config(); 
 
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-// get request for a roast
-app.get("/api/roast", (req, res) => {
-    res.send("hello world")
+// schema for collection in db
+const roastSchema = new mongoose.Schema(
+    {
+        joke: {
+            type: String,
+            require: true,
+        }
+    }
+);
+const Roast = mongoose.model("Roast", roastSchema)
+
+// get request for random joke roast
+app.get("/api/roast", async (req, res) => {
+    let random = await Roast.aggregate().sample(1)
+    res.json({status: 200, roast: random[0].roast});
 })
 
-
+// launch app and connect to db
 app.listen(process.env.PORT, () => {
     mongoose.set("strictQuery", false);
     mongoose
@@ -22,6 +34,5 @@ app.listen(process.env.PORT, () => {
         .catch((e) => { // print error
             console.log(e);
         });
-
     console.log(`roast api listening to ${process.env.PORT}`)
 })
